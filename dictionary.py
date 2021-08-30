2f5511867c6189f25e24e2d0aed6ecddb3045908
Mateusz_Prokop <mateusz.prokopp@gmail.com>
2021-08-30 22:03:56 +0200

    adding definitions feature

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,35 @@
-print("Menu")
-print("1. Dodaj definicję")
-print("2. Wyszukaj definicję")
-print("3. Usuń definicję")
+definitions = {}
+
+while(True):
+    print()
+    print("Menu")
+    print("1. Dodaj definicję")
+    print("2. Wyszukaj definicję")
+    print("3. Usuń definicję")
+    print("4. Pokaż słownik")
+    print("5. Zakończ")
+    print()
+
+    choice = input("Wybierz działanie: ")
+
+    if (choice == "1"):
+        key = input("Podaj słowo do zdefiniowania: ")
+        definition = input("Podaj definicję: ")
+        definitions[key] = definition
+        print("Definicja została pomyślnie dodana!")
+    
+    if (choice == "2"):
+        print("Wybrałeś 2")
+    
+    if (choice == "3"):
+        print("Wybrałeś 3")
+
+    if (choice == "4"):
+        print("Wyświetl słownik")
+
+    if (choice == "5"):
+        print("See you soon")
+        break
+
+    else:
+        print("Podałeś niewłaściwą liczbę")
